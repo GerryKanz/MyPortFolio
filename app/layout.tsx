afa53b2fa45b7566ed187500a94e28288ed4db40
gerry <kanzarag@gmail.com>
2025-02-22 19:06:34 +0900

    updated Beta V1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import styles from "./page.module.css"
-// import { FaArrowCircleRight } from "react-icons/fa";
 import Link from "next/link";
 
 
