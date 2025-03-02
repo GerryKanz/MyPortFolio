7f883ce5d1ce135d835308223012baa62efc7ba7
gerry <kanzarag@gmail.com>
2025-03-02 13:12:08 +0900

    v1.0.8

--- app/projects/projectsData.tsx
+++ app/projects/projectsData.tsx
@@ -41,16 +41,15 @@ export const projects = [
 
     },
     {
-        name: 'Nail Salon Booking App (Beta)',
+        name: 'Bird Classication Model Deep Neural Network (Beta)',
 
-        imagePath: '/nailsapp.png',
+        imagePath: '/BirdClassicationModel.png',
         discription: 'Nail salon booking app project built using React Native is an app that similates the booking process. It allows users to create an account, make appointments and cancel appointments when needed',
-        technologies: 'Javascript and ReactNative',
+        technologies: 'Python, TensorFlow Keras Library, MatplotLib, Pandas and Numpy',
         projectLinks: {
             youtubeLink: 'https://youtu.be/iBS_5fEgYm8',
             youtubeLinkEmbedded: "https://www.youtube.com/embed/iBS_5fEgYm8?si=s8etBdnBBvXAroCI&amp;start=12",
-            expoLink: 'https://expo.dev/preview/update?message=updated%20SDK%20for%20compatibility&updateRuntimeVersion=1.0.0&createdAt=2025-01-17T14%3A54%3A52.142Z&slug=exp&projectId=6f5a2d47-8e58-4c17-990b-50b1bb6214f5&group=0bbef6e3-a8b0-410d-aa8b-c1144eeb6f2e',
-            github: 'https://github.com/GerryKanz/nailSalon'
+            github: 'https://github.com/GerryKanz/Final-project-image-classification-model/blob/main/final_project_ML.ipynb'
         }
 
     }
